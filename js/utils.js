933528bff135793ad5adc41caa7120d132aaf05c
tu <nduytu87@gmail.com>
2016-07-10 09:37:48 +0800

    updated

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,41 @@
 ﻿// external js: isotope.pkgd.js
+// init Isotope
+var $grid = $('.grid').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+});
 
+// filter functions
+var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function () {
+        var number = $(this).find('.number').text();
+        return parseInt(number, 10) > 50;
+    },
+    // show if name ends with -ium
+    ium: function () {
+        var name = $(this).find('.name').text();
+        return name.match(/ium$/);
+    }
+};
+
+// bind filter button click
+$('#filters').on('click', 'a', function () {
+    var filterValue = $(this).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[filterValue] || filterValue;
+    $grid.isotope({ filter: filterValue });
+});
+
+
+// change is-checked class on buttons
+$('.button-group').each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'a', function () {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+    });
+});
 $(document).ready(function () {
 
     $.getJSON('data/portfolio.json', function (data) {
@@ -65,46 +101,11 @@ $(document).ready(function () {
             var contentModal = modalPortfolio.join('');
             listPortfolio.append(content);
             listModal.append(contentModal);
+            $grid.isotope();
         }
     });
 
-    // init Isotope
-    var $grid = $('.grid').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-
-    // filter functions
-    var filterFns = {
-        // show if number is greater than 50
-        numberGreaterThan50: function () {
-            var number = $(this).find('.number').text();
-            return parseInt(number, 10) > 50;
-        },
-        // show if name ends with -ium
-        ium: function () {
-            var name = $(this).find('.name').text();
-            return name.match(/ium$/);
-        }
-    };
-
-    // bind filter button click
-    $('#filters').on('click', 'a', function () {
-        var filterValue = $(this).attr('data-filter');
-        // use filterFn if matches value
-        filterValue = filterFns[filterValue] || filterValue;
-        $grid.isotope({ filter: filterValue });
-    });
-
-
-    // change is-checked class on buttons
-    $('.button-group').each(function (i, buttonGroup) {
-        var $buttonGroup = $(buttonGroup);
-        $buttonGroup.on('click', 'a', function () {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $(this).addClass('is-checked');
-        });
-    });
+   
 
 });
 
