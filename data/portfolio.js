5ef98ccef566f22e8ffc93aba66f21dde70f73ce
Tu <nduytu87@gmail.com>
2016-08-25 00:21:22 +0800

    updated

---
+++ data/portfolio.js
@@ -0,0 +1,62 @@
+﻿{
+    "portfolios": [
+      {
+          "id":"pedigree",
+          "name":"Pedigree responsive email",
+          "class": "email",
+          "image": ["pedigree.jpg", "m_pedigree.jpg"],
+          "description":"Pedigree responsive email"
+      },{
+          "id":"cesar",
+          "name":"Cesar",
+          "class": "email",
+          "image": ["cesar.jpg", "m_cesar.jpg"],
+          "description":"Cesar responsive email"
+      },{
+          "id":"iams",
+          "name":"IAMS",
+          "class": "email",
+          "image": ["iams.jpg", "m_iams.jpg"],
+          "description":"IAMS responsive email"
+      }
+      ,{
+          "id":"em_frontier",
+          "name":"Frontier",
+          "class": "email",
+          "image": ["frontier.jpg"],
+          "description":"Frontier email"
+      }
+      ,{
+          "id":"lp_frontier",
+          "name":"Frontier Landing Page",
+          "class": "landing",
+          "image": ["lp_frontier.jpg","lp_m_frontier.jpg"],
+          "description":"Frontier Landing Page"
+      },{
+          "id":"tg_site",
+          "name":"Treat Them Good",
+          "class": "web",
+          "image": ["TTG_site.jpg","m_TTG_site.jpg"],
+          "description":"Treat Them Good"
+      }
+      ,{
+          "id":"great28",
+          "name":"Frontier Great 28",
+          "class": "web",
+          "image": ["great28.jpg"],
+          "description":"Frontier Great 28"
+      },{
+          "id":"bnr_frontier",
+          "name":"Banner Frontier",
+          "class": "banner",
+          "image": ["bnr_frontier1.jpg","bnr_frontier2.jpg","bnr_frontier3.jpg","bnr_frontier4.jpg"],
+          "description":"Banner Frontier"
+      },{
+          "id":"bnr_daiichi",
+          "name":"Daiichi Sankyo",
+          "class": "banner",
+          "image": ["bnr_daiichi1.jpg","bnr_daiichi2.jpg","bnr_daiichi3.jpg","bnr_daiichi4.jpg"],
+          "description":"Banner Daiichi Sankyo"
+      }
+    ]
+}
